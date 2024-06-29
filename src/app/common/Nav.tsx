bed2c73cd363c880309c5630166af6de70fad235
rladudgh321 <118824790+rladudgh321@users.noreply.github.com>
2024-06-29 13:59:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/common/Nav.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <nav>
+      <h2 className="text-6xl text-center">전체 보기</h2>
+      <ol className="flex flex-wrap w-[600px] justify-center mx-auto my-10">
+        <li className="inline-block p-2">전체보기</li>
+        <li className="inline-block p-2">맛집</li>
+        <li className="inline-block p-2">인생</li>
+        <li className="inline-block p-2">끄적끄적</li>
+        <li className="inline-block p-2">끄적끄적</li>
+        <li className="inline-block p-2">끄적끄적</li>
+        <li className="inline-block p-2">끄적끄적</li>
+        <li className="inline-block p-2">끄적끄적</li>
+        <li className="inline-block p-2">끄적끄적</li>
+        <li className="inline-block p-2">끄적끄적</li>
+        <li className="inline-block p-2">끄적끄적</li>
+        <li className="inline-block p-2">끄적끄적</li>
+        <li className="inline-block p-2">끄적끄적</li>
+        <li className="inline-block p-2">끄적끄적</li>
+        <li className="inline-block p-2">끄적끄적</li>
+        <li className="inline-block p-2">끄적끄적</li>
+      </ol>
+    </nav>
+  );
+};
